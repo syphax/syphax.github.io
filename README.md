117b0b1e35e21b7d749ab7962f9af6e88ab1b9ab
Brian Crounse <bcrounse@wayfair.com>
2020-09-10 22:49:59 -0400

    Added better randomization logic; debugged logic errors with ints

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Top 10 Trump Quotes
 
-Simple public website
+Simple website to generate random quotes from a hard-coded array.
+
+Just for fun. I do not claim to be proficient in JavaScript; don't judge.
+Code was patched together from searching for "how to do {X} in JavaScript".
 
 Credits:
 * https://github.com/romeojeremiah/quote-generator/
+* https://dev.to/ycmjason/how-to-create-range-in-javascript-539i
 
--- scripts.js
+++ scripts.js
@@ -4,16 +4,67 @@
 //Use an array to hold the value of the quotes
 const arrayOfQuotes = [
     {'author': 'Donald Trump, November 6, 2012',
-     'quote': 'The electoral college is a disaster for a democracy.'
-    {'author': '2',
-     'quote': '2'
-    },
+     'quote': 'The electoral college is a disaster for a democracy.'},
+    {'author': 'Donald Trump, March 19, 2020',
+     'quote': 'I wanted to always play it down. I still like playing it down. Yes, because I don’t want to create a panic.'},
+    {'author': 'Donald Trump, Feb. 28, 2020',
+     'quote': 'It’s going to disappear. One day, it’s like a miracle, it will disappear.'},
+    {'author': '4',
+     'quote': '4'},
 ];
 
 function generateQuote(){
-    const random = Number.parseInt(Math.random()*arrayOfQuotes.length + 1);
-    document.querySelector('#quoteOutput-1').textContent = `\"${arrayOfQuotes[random].quote}\"`;
-    document.querySelector('#authorOutput-1').textContent = `--${arrayOfQuotes[random].author}`;
 
-    alert('Hello test!')
+    len_quotes = arrayOfQuotes.length
+    const random = Number.parseInt(Math.random()*arrayOfQuotes.length);
+
+    quote_index = random
+
+    range = range(0, len_quotes - 1)
+    random_sequence = shuffle(range)
+
+    for (i = 1; i <= 3; i++) {
+
+        console.log(random_sequence[i-1])
+
+        j = random_sequence[i-1]
+
+        i_str = i.toString()
+
+        document.querySelector('#quoteOutput-'.concat(i_str)).textContent = `\"${arrayOfQuotes[j].quote}\"`;
+        document.querySelector('#authorOutput-'.concat(i_str)).textContent = `--${arrayOfQuotes[j].author}`;
+
+        quote_index++
+        if (quote_index >= len_quotes) { quote_index = 0 }
+
+    }
+
+}
+
+// Lifted from https://dev.to/ycmjason/how-to-create-range-in-javascript-539i
+
+function range(start, end) {
+    if(start === end) return [start];
+    return [start, ...range(start + 1, end)];
+}
+
+// Lifted from https://stackoverflow.com/questions/18806210/generating-non-repeating-random-numbers-in-js
+
+function shuffle(array) {
+    var i = array.length,
+        j = 0,
+        temp;
+
+    while (i--) {
+
+        j = Math.floor(Math.random() * (i+1));
+
+        // swap randomly chosen element with current element
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
+    return array;
 }
